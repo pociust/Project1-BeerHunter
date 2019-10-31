5ab7084b878ee6dd42c866ecea43bd95faa83e3f
Tom Pocius <poci14@gmail.com>
2019-10-30 22:46:48 -0500

    corrected

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// google maps api key AIzaSyBZrGx2lk-aBzNw1Y5aR-f4DuzZP_a1v2g || key=AIzaSyBZrGx2lk-aBzNw1Y5aR-f4DuzZP_a1v2g
 var city = "";
 var state = "";
 //initMap(city);
@@ -40,6 +39,7 @@ function searchBrewery() {
     console.log(response);
     initMap(city);
     renderList(response);
+    // pinInMap(response[i]);
   });
 }
 
@@ -82,15 +82,12 @@ function initMap(city) {
 
   var options = {
     center: /* { lat: 0, lng: 0 },*/ { lat: 41.8781, lng: -87.6298 },
-    zoom: 14
+    zoom: 11
   };
   //intialize id
   map = new google.maps.Map(document.getElementById("map"), options);
 
-  geocoder.geocode({ address: `${city ? city : state}` }, function(
-    results,
-    status
-  ) {
+  geocoder.geocode({ address: `${city ? city : state}` }, function(results, status) {
     if (status === "OK") {
       map.setCenter(results[0].geometry.location);
     } //else {
@@ -100,54 +97,107 @@ function initMap(city) {
 
   var input = document.getElementById("search");
   //search box auto completes what you are trying see also auto complete
-  var searchBox = new google.maps.places.SearchBox(input);
+  //var searchBox = new google.maps.places.SearchBox(input);
 
   //function to make search results bias in maps current viewport
   //add lisenter to map see events page at https://developers.google.com/maps/documentation/javascript/events
-  map.addListener("bounds_changed", function() {
-    //set the bounds on search box to the bounds of the map
-    searchBox.setBounds(map.getBounds());
-  });
+  // map.addListener("bounds_changed", function() {
+  //set the bounds on search box to the bounds of the map
+  // searchBox.setBounds(map.getBounds());
+  // });
 
   //see search results on map
-  var marker = [];
+  //var marker = [];
 
-  searchBox.addListener("places_changed", function() {
-    // call back will run when user selects place from list
-    var places = searchBox.getPlaces();
+  //searchBox.addListener("places_changed", function() {
+  // call back will run when user selects place from list
+  //var places = searchBox.getPlaces();
 
-    if (places.length === 0) return;
+  //if (places.length === 0) return;
 
-    //clear out all markers used in interation
-    // marker.forEach(function(m) {
-    //   m.setMap(null);
-    // });
-    // marker = [];
+  //clear out all markers used in interation
+  // marker.forEach(function(m) {
+  //   m.setMap(null);
+  // });
+  // marker = [];
 
-    // create bounds object or coordinate boundaries of map
-    var bounds = new google.maps.LatLngBounds();
+  // create bounds object or coordinate boundaries of map
+  //var bounds = new google.maps.LatLngBounds();
 
-    //iterate over places add marker for earch and adjust bounds of map
-    places.forEach(function(p) {
-      //check for geo attr or data for postion; if not
-      if (!p.geometry) return;
+  //iterate over places add marker for earch and adjust bounds of map
+  //places.forEach(function(p) {
+  //check for geo attr or data for postion; if not
+  //if (!p.geometry) return;
 
-      marker.push(
-        new google.maps.Marker({
-          map: map,
-          title: p.name,
-          position: p.geometry.location
-        })
-      );
+  // marker.push(
+  // new google.maps.Marker({
+  // map: map,
+  //title: p.name,
+  //position: p.geometry.location
+  // })
+  // );
 
-      //update bounds of map to take each place into account
-      //check if place had geometry view port geomerty ref https://developers.google.com/maps/documentation/javascript/geometry (if it preffered set to combination ref https://developers.google.com/maps/documentation/javascript/reference/coordinates#LatLngBounds)
-      if (p.geometry.viewport) bounds.union(p.geometry.viewport);
-      else bounds.extend(p.geometry.location);
-    });
+  //update bounds of map to take each place into account
+  //check if place had geometry view port geomerty ref https://developers.google.com/maps/documentation/javascript/geometry (if it preffered set to combination ref https://developers.google.com/maps/documentation/javascript/reference/coordinates#LatLngBounds)
+  // if (p.geometry.viewport) bounds.union(p.geometry.viewport);
+  // else bounds.extend(p.geometry.location);
+  //});
 
-    //call fit bounds on map object and pass in bounds
-    map.fitBounds(bounds);
+  //call fit bounds on map object and pass in bounds
+  // map.fitBounds(bounds);
+  // });
+  $(document).on("click", ".cardResults", function() {
+    var place = $(this).attr("data-name");
+    console.log("place", place);
+
+    var icon = {
+      url: "./smbottle.png", // url
+      scaledSize: new google.maps.Size(13, 34), // scaled size
+      origin: new google.maps.Point(0, 0), // origin
+      anchor: new google.maps.Point(0, 0) // anchor
+    };
+
+    function codeAddress() {
+      geocoder.geocode({ address: place }, function(results, status) {
+        if (status == "OK") {
+          map.setCenter(results[0].geometry.location);
+          var marker = new google.maps.Marker({
+            map: map,
+            draggable: false,
+            position: results[0].geometry.location,
+            icon: icon
+          });
+          console.log("result map", results[0]);
+          marker.setMap(map);
+          var contentString = $(
+            '<div class="marker-info-win">' +
+              '<span class="info-content">' +
+              `<h3 class="marker-heading">${place}</h3>` +
+              "This is a new marker infoWindow" +
+              "</span>" +
+              '<br/><button name="remove-marker" class="remove-marker" title="Remove Marker">Remove Marker</button>' +
+              "</div>"
+          );
+
+          //Create an infoWindow
+          var infowindow = new google.maps.InfoWindow();
+
+          //set the content of infoWindow
+          infowindow.setContent(contentString[0]);
+
+          //add click event listener to marker which will open infoWindow
+          google.maps.event.addListener(marker, "click", function() {
+            infowindow.open(map, marker); // click on marker opens info window
+          });
+          var removeBtn = contentString.find("button.remove-marker")[0];
+          google.maps.event.addDomListener(removeBtn, "click", function(event) {
+            marker.setMap(null);
+          });
+        }
+      });
+    }
+
+    codeAddress();
   });
 }
 
@@ -161,45 +211,28 @@ function pinInMap(brewery) {
       });
       marker.setMap(map);
       console.log(brewery.name);
+      var contentString = $(
+        '<div class="marker-info-win">' +
+          '<span class="info-content">' +
+          `<h3 class="marker-heading">${brewery.name}</h3>` +
+          "This is a new marker infoWindow" +
+          "</span>" +
+          "</div>"
+      );
+
+      //Create an infoWindow
+      var infowindow = new google.maps.InfoWindow();
+
+      //set the content of infoWindow
+      infowindow.setContent(contentString[0]);
+
+      //add click event listener to marker which will open infoWindow
+      google.maps.event.addListener(marker, "click", function() {
+        infowindow.open(map, marker); // click on marker opens info window
+      });
     }
   });
 }
-
-//   $(document).on("click", ".cardResults", function() {
-//     var lat = parseFloat($(this).attr("data-lat"));
-//     var lon = parseFloat($(this).attr("data-lon"));
-//     var place = $(this).attr("data-name");
-
-//     console.log("lat", lat);
-//     console.log("lon", lon);
-//     console.log("place", place);
-
-//     var icon = {
-//       url: "./smbottle.png", // url
-//       scaledSize: new google.maps.Size(13, 34), // scaled size
-//       origin: new google.maps.Point(0, 0), // origin
-//       anchor: new google.maps.Point(0, 0) // anchor
-//     };
-
-//     codeAddress();
-
-//     function codeAddress() {
-//       geocoder.geocode({ address: place }, function(results, status) {
-//         if (status == "OK") {
-//           map.setCenter(results[0].geometry.location);
-//           var marker = new google.maps.Marker({
-//             map: map,
-//             draggable: false,
-//             position: results[0].geometry.location,
-//             icon: icon
-//           });
-//           console.log("result map", results[0]);
-//           marker.setMap(map);
-//         }
-//       });
-//     }
-//   });
-// }
 
 $("#startBtn").click(function() {
   $("html,body").animate(
@@ -214,9 +247,15 @@ $("form").submit(function(event) {
   event.preventDefault();
   $("#resultBrew").html("");
   searchBrewery();
+  $("html,body").animate(
+    {
+      scrollTop: $("#mapContainer").offset().top
+    },
+    "slow"
+  );
 });
 
-// initMap();
+//initMap();
 //init card
 //show card with imputs city state
 //drop for number of results
